3cc44e71c716a0c1f5415b7820c8219c82e747a7
otigooh@2014 <cygu@aims.ac.za>
2019-02-07 11:27:08 -0500

    More descriptives

--- aphrc/autopsy/generalsymptoms.R
+++ aphrc/autopsy/generalsymptoms.R
@@ -63,6 +63,35 @@ symptoms_plot <- (symptoms_colper_tab
 )
 symptoms_plot
 
+
+#### ---- Other forms of illness before death ----
+
+colvars <- "slumarea"
+rowvars <- grouped_vars[["b4dth_var"]]
+b4dth_dist <- (working_df
+ %>% multiresTabs(colvars, rowvars)
+)
+b4dth_count_tab <- b4dth_dist[["multicount_tab"]]
+b4dth_colper_tab <- b4dth_dist[["multicolper_tab_long"]]
+b4dth_rowper_tab <- b4dth_dist[["multirowper_tab_long"]]
+
+legend_title <- "EA/Total"
+plot_title <- "Prevalence"
+cols <- c("Total" = "red4", "korogocho" = "green4", "viwandani" = "blue4")
+b4dth_plot <- (b4dth_colper_tab
+ %>% multiresPlot(legend_title, plot_title)
+ + scale_fill_manual(
+ 	values = cols
+	, breaks = c("Total", "korogocho", "viwandani")
+	, labels = c("Total", "korogocho", "viwandani")
+ )
+ + ylab("Percentage")
+ + xlab("Symptoms")
+)
+b4dth_plot
+
+
+
 # Save objects
 symptoms_saved_tabs <- sapply(grep("_tab$", ls(), value = TRUE), get)
 symptoms_saved_plots <- sapply(grep("_plot$", ls(), value = TRUE), get)
