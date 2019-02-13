74e9943d623cda2fca6177e3fc179e442317ab4e
otigooh@2014 <cygu@aims.ac.za>
2019-02-13 16:15:09 -0500

    Edited the GLMM model

---
+++ aphrc/wash/clustering.R
@@ -0,0 +1,31 @@
+#### ---- Project: APHRC Wash Data ----
+#### ---- Task: Clustering ----
+#### ---- By: Steve and Jonathan ----
+#### ---- Date: 2019 Feb 13 (Wed) ----
+
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+load("globalFunctions.rda")
+load("descriptivePlots.rda")
+
+#Since the clustering data contains both categorical and numeric variables, we can not therefore use the k-modes clustering. K-medoids clustering allows both numeric and categorical data.
+
+#The goal is to find k representative objects which minimize the sum of the dissimilarities of the observations to their closest representative object.
+
+#### ---- Determine k ----
+
+factor_vars <- c("hhid_anon"
+	, "slumarea", "gender", "ethnicity"
+)
+ordered_factors_vars <- c("hhdhungerscale"
+	, "wealthquintile", "cat_hhwatersource"
+	, "cat_hhtoilettype", "cat_hhgarbagedisposal"
+)
+numeric_vars <- c("intvwyear"
+	, "ageyears", "numpeople_total"
+	, "expend_total_USD_per_centered"
+)
+
+cluster_df <- select
